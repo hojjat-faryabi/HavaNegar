0a5cba54c9b6a950c40ac434acdd5d6a47b3b9bb
Hojjat Faryabi <hojjat.faryabi@gmail.com>
2019-07-25 01:15:31 +0430

    test api key

---
+++ lib/services/api_key.dart
@@ -0,0 +1,8 @@
+class ApiKey{
+  static String geoKey = "18647804361599821533x3073";
+  // i use geocode.xyz api
+
+
+  static String weatherKey = "e573269fa5a324e904e68cc19256b02f";
+  // i use dark sky api
+}
