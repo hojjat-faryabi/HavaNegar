bdaed79a1edcec1537c14a04c69ccba4ffce4724
Hojjat Faryabi <hojjat.faryabi@gmail.com>
2019-07-31 01:16:20 +0430

    add farsi to app

---
+++ lib/utility/app_language.dart
@@ -0,0 +1,45 @@
+class FarsiNames {
+  /// week days with number
+  static Map weekNumbers = {
+    0 : "شنبه",
+    1 : "یک شنبه",
+    2 : "دو شنبه",
+    3 : "سه شنبه",
+    4 : "چهار شنبه",
+    5 : "پنج شنبه",
+    6 : "جمعه",
+  };
+
+  /// AM or PM
+  static Map halfDay = {
+    "am" : "صبح",
+    "AM" : "صبح",
+    "pm" : "عصر",
+    "PM" : "عصر",
+  };
+
+  /// Iran city's name
+  static Map iranCity = {
+    // ostan alborz
+    "Alborz" : "",
+    "Karaj" : "",
+
+  };
+
+  /// icon of weathers /// weather status
+  static Map weatherStatus = {
+    "clear-day" : "صـاف",
+    "clear-night" : "صـاف",
+    "rain" : "بارانی",
+    "snow" : "برفی",
+    "sleet" : "بوران",
+    "wind" : "نسیم",
+    "fog" : "مـه",
+    "cloudy" : "ابری",
+    "partly-cloudy-day" : "نیمه ابری",
+    "partly-cloudy-night" : "نیمه ابری",
+    "hail" : "تگرگ",
+    "thunderstorm" : "رعدوبرق",
+    "tornado" : "گردباد",
+  };
+}
