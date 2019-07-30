503fef0b2e75e61c936360c67fea8cdac406e892
Hojjat Faryabi <hojjat.faryabi@gmail.com>
2019-07-31 01:19:42 +0430

    in splash screen only need initial this file

---
+++ lib/utility/initial_data.dart
@@ -0,0 +1,50 @@
+/// this data fetch from data base in splash screen
+
+class InitialData {
+  /// initial data section
+  static bool isDarkMode = false;
+  static Map lastUpdate = {
+    "clock" : "8:20",
+    "am_pm" : "pm"
+  };
+
+  /// homePage data section
+  static String cityName = "Jiroft";
+  static String highTemp = "40";
+  static String lowTemp = "30";
+  static String temperature = "38";
+  static String sunRise = "5:40";
+  static String sunSet = "7:20";
+  static String wind = "20";
+  static String weatherStatus = "suny";
+
+
+  /// drawer data section
+  static Map day1 = {
+    "icon" : "rainy_icon.svg",
+    "week_number" : 0,
+    "high_temp" : 35,
+    "low_temp" : 15,
+  };
+
+  static Map day2 = {
+  "icon" : "rainy_icon.svg",
+  "week_number" : 1,
+  "high_temp" : 35,
+  "low_temp" : 15,
+  };
+
+  static Map day3 = {
+    "icon" : "rainy_icon.svg",
+    "week_number" : 2,
+    "high_temp" : 35,
+    "low_temp" : 15,
+  };
+
+  static Map day4 = {
+    "icon" : "rainy_icon.svg",
+    "week_number" : 3,
+    "high_temp" : 35,
+    "low_temp" : 15,
+  };
+}
