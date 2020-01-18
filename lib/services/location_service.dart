79be70e4f6703d1210c36e561402903231372140
Hojjat Faryabi <hojjat.faryabi@gmail.com>
2020-01-18 22:50:37 +0330

    add permission section to location service

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,24 +1,33 @@
-
 import 'package:location/location.dart';
 
 class LocationService {
+  static Future<Map> getLocation() async {
+    var location = new Location();
 
+    LocationData currentLocation;
 
-  static getLocation() async{
-        var location = new Location();
-
-  LocationData currentLocation ;
-
-      print(await location.hasPermission());
-      try {
-        currentLocation = await location.getLocation();
-        print("----------------------" + currentLocation.latitude.toString());
-      } catch (e) {
-        print(e.toString());
-        if (e.code == 'PERMISSION_DENIED') {
-          print('Permission denied');
-        }
-        currentLocation = null;
+    try {
+      currentLocation = await location.getLocation();
+      print("----------------------" + currentLocation.latitude.toString());
+      return {
+        "lat" : "",
+        "lon" : "",
+      };
+    } catch (e) {
+      print(e.toString());
+      if (e.code == 'PERMISSION_DENIED') {
+        print('Permission denied');
       }
+      return null;
+    }
   }
+
+  static Future<bool> getPermission() async{
+    return await Location().requestPermission();
+  }
+
+  static Future<bool> hasPermission() async{
+    return await Location().hasPermission();
+  }
+
 }
